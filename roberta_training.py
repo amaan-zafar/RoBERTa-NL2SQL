077fcddd79892e073d756cf38b8540cea18a474a
Amaan Zafar <53682426+amaan-zafar@users.noreply.github.com>
2023-09-08 00:29:30 +0530

    Update roberta_training.py

--- roberta_training.py
+++ roberta_training.py
@@ -243,12 +243,12 @@ def get_wemb_n(i_nlu, l_n, hS, num_hidden_layers, all_encoder_layer, num_out_lay
             i_layer = num_hidden_layers - 1 - i_noln
             st = i_noln * hS
             ed = (i_noln + 1) * hS
-            wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][b, i_nlu1[0]:i_nlu1[1], :]
             print("Type of wemb_n:", type(wemb_n))
             print("Type of all_encoder_layer[i_layer]:", type(all_encoder_layer[i_layer]))
             print("Type of i_nlu1:", type(i_nlu1))
             print("Type of st:", type(st))
             print("Type of ed:", type(ed))
+            wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][b, i_nlu1[0]:i_nlu1[1], :]
 
     return wemb_n
 
